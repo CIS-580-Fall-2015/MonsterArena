9a23bbfd5e0b6818b2101ee29ac5d217c380ed85
Richard Petrie <rap1011@ksu.edu>
2015-12-12 17:39:09 -0600

    spawning initial

--- src/entity_manager.js
+++ src/entity_manager.js
@@ -3,14 +3,17 @@ module.exports = (function() {
   var Hero = require('./hero.js');
   var Door = require('./spawner');
   var doors = [];
-  var stat_cap = {"attack": 2, "defense": 2, "health": 2};
-  var stat_levels = [2, 3, 5, 7, 10, 12, 15];
-  var blood = 0;
+  var monsters = [];
 
   var hero;
+
   // Value followed by scaling
-  var HERO_STATS = {health: [2, 1.2], attack: [3, 1.2], defense: [2, 1.1], exp: [0, 1.2]};
-  var monsters = [];
+  var HERO_STATS = {
+    health: [2, 1.2],
+    attack: [3, 1.2],
+    defense: [2, 1.1],
+    exp: [0, 1.2]
+  };
 
   function initialize() {
     //TODO spawn doors
@@ -22,34 +25,27 @@ module.exports = (function() {
     //TODO generate information to render
   }
 
-  function update_cap(stat, level) {
-    //TODO error catching
-    stat_cap[stat] = stat_levels.indexOf(level);
-  }
-
   //TODO open doors upgrade
 
-  //TODO monster spawning
-
-  function mod_blood(amount) {
-    blood += amount;
-    if (blood < 0) {
-      blood = 0;
+  function spawn_monster(stats) {
+    var d = null;
+    for (var i = 0; i > 8; i++) {
+      if (doors[i].open && doors[i].avaliable) {
+        d = doors[i];
+        break;
+      }
+    }
+    if (d) {
+      var m = new Monster(stats, d);
+      d.avaliable = false;
+      monsters.push(m);
     }
   }
-
-  function get_blood() {
-    return blood;
-  }
-
-
 
   return {
     initialize: initialize,
     update: update,
-    update_cap: update_cap,
-    mod_blood: mod_blood,
-    get_blood: get_blood,
+    spawn_monster: spawn_monster,
   };
 
 }());
--- src/spawner.js
+++ src/spawner.js
@@ -1,11 +1,11 @@
-module.exports = (function(){
+module.exports = (function() {
 
   function Door(x, y, id) {
     this.x = x;
     this.y = y;
     this.id = id;
     this.open = false;
-    this.monster = false;
+    this.avaliable = false;
   }
 
   return Door;
