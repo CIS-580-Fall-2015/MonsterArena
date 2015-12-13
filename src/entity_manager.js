87a6009bc9783313b3bbfb2ef01367bda723a1c0
Richard Petrie <rap1011@ksu.edu>
2015-12-12 18:49:03 -0600

    Simple door and hero spawning
    May need tweaks.

--- src/entity_manager.js
+++ src/entity_manager.js
@@ -4,6 +4,7 @@ module.exports = (function() {
   var Door = require('./spawner');
   var doors = [];
   var monsters = [];
+  var unlocked_doors = 1;
 
   var hero;
 
@@ -15,22 +16,37 @@ module.exports = (function() {
     exp: [0, 1.2]
   };
 
-  function initialize() {
-    //TODO spawn doors
+  var ARENA_WIDTH; //TODO
+  var ARENA_HEIGHT; //TODO
+  var OFFSET = 64;
 
-    hero = new Hero(HERO_STATS, this);
+  function initialize() {
+    doors[0] = new Door(ARENA_WIDTH / 2, OFFSET); // North
+    doors[1] = new Door(ARENA_WIDTH - OFFSET, ARENA_HEIGHT / 2); // East
+    doors[2] = new Door(ARENA_WIDTH / 2, ARENA_HEIGHT - OFFSET); //South
+    doors[3] = new Door(OFFSET, ARENA_HEIGHT / 2); // West
+    doors[4] = new Door(ARENA_WIDTH * 0.75, ARENA_HEIGHT * 0.25); // North-East
+    doors[5] = new Door(ARENA_WIDTH * 0.75, ARENA_HEIGHT * 0.75); // South-East
+    doors[6] = new Door(ARENA_WIDTH * 0.25, ARENA_HEIGHT * 0.75); // South-West
+    doors[7] = new Door(ARENA_WIDTH * 0.25, ARENA_HEIGHT * 0.25); // North-West
+
+    hero = new Hero(HERO_STATS, ARENA_WIDTH / 2 - 32, ARENA_HEIGHT / 2 - 32, this);
   }
 
   function update() {
     //TODO generate information to render
   }
 
-  //TODO open doors upgrade
+  function open_door() {
+    if (unlocked_doors < 7) {
+      unlocked_doors++;
+    }
+  }
 
   function spawn_monster(stats) {
     var d = null;
-    for (var i = 0; i > 8; i++) {
-      if (doors[i].open && doors[i].avaliable) {
+    for (var i = 1; i > unlocked_doors; i++) {
+      if (doors[i].avaliable) {
         d = doors[i];
         break;
       }
