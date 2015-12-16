1273506f10807bcf25cd4a0c290d91aaae28a3ec
Nic Johnson <nicjohnson145@hotmail.com>
2015-12-16 14:26:04 -0600

    added js for credits and GO menu states

---
+++ src/credits.js
@@ -0,0 +1,81 @@
+/* Author: Nic Johnson
+ *
+ * Title: credits.js
+ *
+ * Description: 
+ * 
+ *
+ * History:
+ * 	December 16, 2015: 
+ * 		-Date Created
+ */
+module.exports = (function()
+{
+	var menu = document.getElementById("credits");
+	var statemanager;
+	var back = document.getElementById("cred_back_btn");
+	/*
+	* The load() method initializes the menu 
+	* and tells the DOM to render the menu HTML
+	* parameters:
+	* - sm the state manager
+	*/
+	var load = function(sm) 
+	{
+		stateManager = sm;
+		menu.style.display = "flex";
+	}
+
+	/*
+	* The exit() method hides the menu
+	*/
+	var exit = function() 
+	{
+		menu.style.display = "none";
+	}
+
+	/* 
+	* The update() method updates the menu
+	* (in this case, a no-op)
+	*/
+	var update = function() {}
+
+	/* 
+	* The render() method renders the menu
+	* (in this case, a no-op as the menu is 
+	* HTML elements renderd by the DOM)
+	*/
+	var render = function() {}
+
+	back.onclick = function(e)
+	{
+		statemanager.popState();
+	}
+
+	/* 
+	* The keyDown() method handles 
+	* the key down event for the menu.
+	*/
+	var keyDown = function(event) 
+	{
+		// switch(event.keyCode) {
+		//   case 13: // ENTER
+		//     event.preventDefault();
+		// 	    stateManager.popState();
+		//     break;
+		// }
+	}
+
+	/* The keyUp() method handles the key up event */
+	function keyUp(event) {}
+
+	return {
+	    load: load,
+	    exit: exit,
+	    update: update,
+	    render: render,
+	    keyDown: keyDown,
+	    keyUp: keyUp,
+    }
+
+})();
---
+++ src/game_over.js
@@ -0,0 +1,83 @@
+/* Author: Nic Johnson
+ *
+ * Title: game_over.js
+ *
+ * Description: 
+ * 
+ *
+ * History:
+ * 	December 16, 2015: 
+ * 		-Date Created
+ */
+module.exports = (function()
+{
+	var menu = document.getElementById("game_over");
+	var statemanager;
+	var credits = document.getElementById("GO_credits_btn");
+	/*
+	* The load() method initializes the menu 
+	* and tells the DOM to render the menu HTML
+	* parameters:
+	* - sm the state manager
+	*/
+	var load = function(sm) 
+	{
+		stateManager = sm;
+		menu.style.display = "flex";
+	}
+
+	/*
+	* The exit() method hides the menu
+	*/
+	var exit = function() 
+	{
+		menu.style.display = "none";
+	}
+
+	/* 
+	* The update() method updates the menu
+	* (in this case, a no-op)
+	*/
+	var update = function() {}
+
+	/* 
+	* The render() method renders the menu
+	* (in this case, a no-op as the menu is 
+	* HTML elements renderd by the DOM)
+	*/
+	var render = function() {}
+
+	credits.onclick = function(e)
+	{
+		e.preventDefault();
+		var c = require('./credits.js');
+		statemanager.pushState(c);
+	}
+
+	/* 
+	* The keyDown() method handles 
+	* the key down event for the menu.
+	*/
+	var keyDown = function(event) 
+	{
+		// switch(event.keyCode) {
+		//   case 13: // ENTER
+		//     event.preventDefault();
+		// 	    stateManager.popState();
+		//     break;
+		// }
+	}
+
+	/* The keyUp() method handles the key up event */
+	function keyUp(event) {}
+
+	return {
+	    load: load,
+	    exit: exit,
+	    update: update,
+	    render: render,
+	    keyDown: keyDown,
+	    keyUp: keyUp,
+    }
+
+})();
