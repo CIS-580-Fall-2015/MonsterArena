0c5c591a5e1f4e774736c3da3aee1c419dce27e5
Nic Johnson <nicjohnson145@hotmail.com>
2015-12-16 18:34:29 -0600

    forgot to broswerify

--- MonsterArena.js
+++ MonsterArena.js
@@ -2096,6 +2096,7 @@ module.exports = (function()
 	function AddGold(amt)
 	{
 		var val = amt || defaultAddition;
+		val = Math.floor(val);
 		totalGold += val;
 		UpdateItemGrey();
 		SetGoldText();
